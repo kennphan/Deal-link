0b53980ba10be5eacae14d37af41c34c33cf20db
kennphan <ken.n.phan@gmail.com>
2014-09-08 19:11:34 +1000

    create and link all screens (prototype)

--- Deal-link/MenuController.swift
+++ Deal-link/MenuController.swift
@@ -21,16 +21,4 @@ class MenuController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func mapIconPressed(sender: AnyObject) {
-        let mapView = self.storyboard.instantiateViewControllerWithIdentifier("mapView") as MapController
-        self.navigationController.pushViewController(mapView, animated: true)
-        
-    }
-    
-    @IBAction func messagesIconPressed(sender: AnyObject) {
-        let messagesView = self.storyboard.instantiateViewControllerWithIdentifier("messagesView") as MessagesController
-        self.navigationController.pushViewController(messagesView, animated: true)
-    }
-    
-    
 }
